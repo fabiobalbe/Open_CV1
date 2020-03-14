72a9918415a8a0d159f0d07fde34189e51ce68f4
Fábio Balbé <contato@fabiobalbe.com>
2020-03-14 18:17:26 -0300

    Atualização no arq. 07

--- 07 - Drawing on an image.py
+++ 07 - Drawing on an image.py
@@ -12,5 +12,22 @@ output = image.copy()
 cv2.rectangle(output, (260, 20), (360, 120), (0, 0, 255), 2)
 cv2.imshow("Rectangle", output)
 
+# draw a blue 20px (filled in) circle on the image centered at
+# x=300,y=150
+output = image.copy()
+cv2.circle(output, (300, 150), 20, (255, 0, 0), -1)
+cv2.imshow("Circle", output)
+
+# draw a 5px thick red line from x=60,y=20 to x=400,y=200
+output = image.copy()
+cv2.line(output, (60, 20), (400, 200), (0, 0, 255), 5)
+cv2.imshow("Line", output)
+
+# draw green text on the image
+output = image.copy()
+cv2.putText(output, "OpenCV + Jurassic Park!!!", (10, 25), 
+	cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
+cv2.imshow("Text", output)
+
 #BREAKE THE LOOP
 cv2.waitKey()
